7587fda87c5cf1d461127ebbab3f7f073559212d
J-Kistner <jk.gm.act@gmail.com>
2026-01-31 14:51:02 -0500

    Added controls for the indexer

--- src/main/java/frc/robot/Constants/RobotConstants.java
+++ src/main/java/frc/robot/Constants/RobotConstants.java
@@ -79,5 +79,7 @@ public class RobotConstants {
         public static final AngularVelocity forwardsOnSpeeds = RPM.of(100);
         /** Motor speed in RPM when reversing/ejecting game pieces (backwards operation). */
         public static final AngularVelocity backwardsOnSpeeds = RPM.of(100);
+        /** Manual slow speed in RPM for manual control. */
+        public static final AngularVelocity manualSpeed = RPM.of(20);
     }
 }
