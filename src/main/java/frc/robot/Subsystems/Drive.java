3ab2b861fda2c31688b08df473a8c08347a516c2
cmacdonaldmuckle <macdonaldmucklecolin@gmail.com>
2026-01-31 16:14:53 -0500

    added getHub and hubDistance methods

--- src/main/java/frc/robot/Subsystems/Drive.java
+++ src/main/java/frc/robot/Subsystems/Drive.java
@@ -4,6 +4,7 @@ import static edu.wpi.first.units.Units.Meter;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Optional;
 import java.util.function.DoubleSupplier;
 import java.util.function.Supplier;
 
@@ -14,6 +15,8 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -282,6 +285,34 @@ public class Drive extends CSubsystem {
      */
     public Command dummyDrivePose() {
         return driveToTargetPose(new Pose2d(12.42, 5.05, Rotation2d.fromDegrees(117.0)), 0.0);
+    }
+
+    // Find our alliance's HUB and the location
+    public Translation2d getHub() {
+        Optional<Alliance> alliance = DriverStation.getAlliance();
+        Translation2d allianceHub = new Translation2d(0,0);
+        if(alliance.isPresent()) {
+            if(alliance.get() == Alliance.Blue) {
+                allianceHub = new Translation2d(182.11, 158.84);
+            }
+            if(alliance.get() == Alliance.Red) {
+                allianceHub = new Translation2d(469.11, 158.84);
+            }
+        }
+
+        return allianceHub;
+    }
+
+    // Calculate the distance that the robot is from our alliance's HUB
+    public double hubDistance() {
+        Translation2d allianceHub = getHub();
+        Pose2d currentPose = getPose();
+
+        double xDistance = Math.abs(currentPose.getX() - allianceHub.getX());
+        double yDistance = Math.abs(currentPose.getY() - allianceHub.getY());
+
+        // return the horizontal distance of the robot from the HUB in feet
+        return Math.sqrt((Math.pow(xDistance, 2) + Math.pow(yDistance, 2))) / 12;
     }
 
     /**
