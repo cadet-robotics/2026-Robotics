58f741621ef2d0eef061f81e026051dee01971ea
J-Kistner <jk.gm.act@gmail.com>
2026-02-28 01:16:29 -0500

    ShooterSim now somewhat reliant on SOTM. not working well

--- src/main/java/frc/robot/Subsystems/Shooter.java
+++ src/main/java/frc/robot/Subsystems/Shooter.java
@@ -22,6 +22,8 @@ import frc.robot.Constants.ShooterState;
 import frc.robot.Libs.CCommand;
 import frc.robot.Libs.CSubsystem;
 import frc.robot.Libs.FuelSim;
+import frc.robot.Libs.ShootOnTheMove;
+
 import static edu.wpi.first.units.Units.MetersPerSecond;
 import static edu.wpi.first.units.Units.Meters;
 import yams.mechanisms.config.FlyWheelConfig;
@@ -174,6 +176,13 @@ public class Shooter extends CSubsystem {
                         lastSpawnNs = System.nanoTime();
                     }
                 })
+                .onExecute(() -> {
+                    if (ShootOnTheMove.shooterRPM.isPresent()) {
+                        shooter_controller.setMechanismVelocitySetpoint(ShootOnTheMove.shooterRPM.get());
+                    } else {
+                        shooter_controller.setMechanismVelocitySetpoint(ShooterSubsystemConstants.forwardsOnSpeeds);
+                    }
+                })
                 .onEnd(() -> {
                     state = ShooterState.Off;
                     shooter_motor_controller.setVoltage(0);
@@ -239,17 +248,12 @@ public class Shooter extends CSubsystem {
             if (lastSpawnNs == 0) lastSpawnNs = now;
             if (now - lastSpawnNs >= intervalNs) {
                 lastSpawnNs = now;
-                // Use the shooter elevation (~70 degrees) and zero turret yaw (robot-relative)
                 try {
-                    fuelSim.launchFuel(MetersPerSecond.of(7), Degrees.of(80.0), Degrees.of(180.0), Meters.of(0.9));
+                    fuelSim.launchFuel(MetersPerSecond.of(shooter_controller.getSpeed().magnitude() * 60 * 8.0 / 3000), Degrees.of(80.0), Degrees.of(180.0), Meters.of(0.9));
                 } catch (IllegalStateException ex) {
                     // Robot not registered with fuelSim yet; ignore spawn attempt
                 }
             }
         }
-        // Publish last computed ideal target each simulation cycle for visualization
-        var tbl = NetworkTableInstance.getDefault().getTable("Shooter");
-        tbl.getEntry("IdealTargetPose").setDoubleArray(new double[] { lastIdealX, lastIdealY, 0.0 });
-        tbl.getEntry("IdealTargetTime").setDouble(lastIdealT);
     }
 }
