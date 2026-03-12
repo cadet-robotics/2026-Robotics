3f6db421dd5e48004382682e049929673054ace6
Firemaster1546 <122168476+Firemaster1546@users.noreply.github.com>
2026-03-12 13:34:05 -0400

    climbHigher

--- src/main/java/frc/robot/Constants/RobotConstants.java
+++ src/main/java/frc/robot/Constants/RobotConstants.java
@@ -192,7 +192,7 @@ public class RobotConstants {
         /** Zero/retracted position of the climber (rotations). */
         public static final double ZERO_POSITION = 0;
         /** Climbing position of the climber - between zero and max (rotations). Tune on robot. */
-        public static final double CLIMB_POSITION = 11.584;
+        public static final double CLIMB_POSITION = 11.0;
         /** Maximum extended position of the climber (rotations). Tune on robot. */
         public static final double MAX_POSITION = 18.23;
     }
